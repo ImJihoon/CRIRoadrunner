3f40aa4df9363dd5b108247aa2381b632885aae1
James <jamesetam@gmail.com>
2025-07-17 08:03:07 -0500

    auto :_

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Vision/NewSampleSearch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Vision/NewSampleSearch.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Autonomous.Vision;
 import static org.opencv.imgproc.Imgproc.COLOR_GRAY2BGR;
 import static org.opencv.imgproc.Imgproc.COLOR_GRAY2RGB;
 import static org.opencv.imgproc.Imgproc.COLOR_GRAY2RGBA;
+import static org.opencv.imgproc.Imgproc.COLOR_RGB2HSV;
 import static org.opencv.imgproc.Imgproc.MORPH_RECT;
 import static org.opencv.imgproc.Imgproc.createHanningWindow;
 import static org.opencv.imgproc.Imgproc.getStructuringElement;
@@ -33,16 +34,23 @@ import java.util.List;
 public class NewSampleSearch implements VisionProcessor {
     VisionPortal frontPortal;
     //cv test stuff
-    public boolean seeThres = false;
+    public boolean seeThres = true;
     public boolean seeMorph = false;
 
     public boolean yellow = false;
     public boolean red = false;
     public boolean blue = false;
+    public Scalar redLower;
+    public Scalar redUpper;
+    public Scalar blueLower;
+    public Scalar blueUpper;
+    public Scalar yellowLower;
+    public Scalar yellowUpper;
 
-    public int erodeSize = 7;
+public int erodeSize1 = 5;
+    public int erodeSize2 = 10;
     public int dilateSize1 = 5;
-    public int dilateSize2 = 2;
+    public int dilateSize2 = 10;
 
     double height; //mm
     HashMap<String, Double> camFOV; //deg, deg
@@ -52,6 +60,7 @@ public class NewSampleSearch implements VisionProcessor {
     double distFromCenter;
     double horiz;
     double l3;
+
 
     double count = -256;
 
@@ -97,6 +106,33 @@ public class NewSampleSearch implements VisionProcessor {
     public Object processFrame(Mat frame, long l) {
         if (count > 0 || count == -256) {
             if (count > 0) count--;
+            Mat ground = frame.submat(new Rect(300, 400, 120, 40));
+            Mat ground_hsv = new Mat();
+
+            Imgproc.cvtColor(ground, ground_hsv, COLOR_RGB2HSV);
+
+            double[] groundAvg = Core.mean(ground_hsv).val;
+
+            Imgproc.putText(frame, String.format("avg Color: %s, %s, %s",
+                            (int)(Core.mean(ground_hsv).val[0]*100)/100.0,
+                            (int)(Core.mean(ground_hsv).val[1]*100)/100.0,
+                            (int)(Core.mean(ground_hsv).val[2]*100)/100.0),
+                    new Point(200,300),
+                    Imgproc.FONT_HERSHEY_SIMPLEX,
+                    0.5, new Scalar(255, 255, 255), 1, Imgproc.LINE_AA);
+            Imgproc.rectangle(frame, new Rect(300, 400, 120, 40), Core.mean(ground), -1);
+            Imgproc.rectangle(frame, new Rect(300, 400, 120, 40), new Scalar(255,255,255), 1);
+
+            //yibe
+            redLower = new Scalar(150,5*groundAvg[1]/3+30,groundAvg[2]*0.9);
+            redUpper = new Scalar(groundAvg[0]/10+7,255,255);
+
+            blueLower = new Scalar(groundAvg[0]*3/4+37.5,groundAvg[1]/3-25+groundAvg[0],10);
+            blueUpper = new Scalar(131,255,-2.5*groundAvg[2]+80+10*groundAvg[0]);
+
+            yellowLower = new Scalar(14,100,groundAvg[2]+10);
+            yellowUpper = new Scalar(groundAvg[0]/2+15,255,255);
+
             Mat img_hsv = new Mat();
             Imgproc.cvtColor(frame, img_hsv, Imgproc.COLOR_RGB2HSV);
 
@@ -106,25 +142,31 @@ public class NewSampleSearch implements VisionProcessor {
             if (red) {
                 Mat red1 = new Mat();
                 Mat red2 = new Mat();
-                Core.inRange(img_hsv, new Scalar(340, 30, 60.0), new Scalar(360, 90, 100.0), red1);
-                Core.inRange(img_hsv, new Scalar(0, 30, 60.0), new Scalar(10, 90, 100.0), red2);
+                Core.inRange(img_hsv, redLower, new Scalar(360, redUpper.val[1], redUpper.val[2]), red1);
+                Core.inRange(img_hsv, new Scalar(0, redLower.val[1], redLower.val[2]), redUpper, red2);
                 Core.bitwise_or(red1, red2, img_threshold);
-
             }
             if (blue) {
                 Mat blue = new Mat();
-                Core.inRange(img_hsv, new Scalar(220, 47, 36.1), new Scalar(235, 66.3, 100), blue);
+                Core.inRange(img_hsv, blueLower, blueUpper, blue);
                 Core.bitwise_or(img_threshold, blue, img_threshold);
 
             }
             if (yellow) {
                 Mat yellow = new Mat();
-                Core.inRange(img_hsv, new Scalar(48, 23.1, 40), new Scalar(62, 85, 100.0), yellow);
+                Core.inRange(img_hsv, yellowLower, yellowUpper, yellow);
                 Core.bitwise_or(img_threshold, yellow, img_threshold);
             }
 
 
             //NOTE: morphology
+
+            Mat erodeKernel1 = getStructuringElement(
+                    MORPH_RECT,
+                    new org.opencv.core.Size(
+                            2 * erodeSize1 + 1,
+                            2 * erodeSize1 + 1),
+                    new Point(erodeSize1, erodeSize1));
             Mat dilateKernel1 = getStructuringElement(
                     MORPH_RECT,
                     new org.opencv.core.Size(
@@ -132,12 +174,12 @@ public class NewSampleSearch implements VisionProcessor {
                             2 * dilateSize1 + 1),
                     new Point(dilateSize1, dilateSize1));
 
-            Mat erodeKernel = getStructuringElement(
+            Mat erodeKernel2 = getStructuringElement(
                     MORPH_RECT,
                     new org.opencv.core.Size(
-                            2 * erodeSize + 1,
-                            2 * erodeSize + 1),
-                    new Point(erodeSize, erodeSize));
+                            2 * erodeSize2 + 1,
+                            2 * erodeSize2 + 1),
+                    new Point(erodeSize2, erodeSize2));
 
             Mat dilateKernel2 = getStructuringElement(
                     MORPH_RECT,
@@ -148,8 +190,9 @@ public class NewSampleSearch implements VisionProcessor {
 
             Mat img_morph = new Mat();
 
-            Imgproc.dilate(img_threshold, img_morph, dilateKernel1);
-            Imgproc.erode(img_morph, img_morph, erodeKernel);
+            Imgproc.erode(img_threshold, img_morph, erodeKernel1);
+            Imgproc.dilate(img_morph, img_morph, dilateKernel1);
+            Imgproc.erode(img_morph, img_morph, erodeKernel2);
             Imgproc.dilate(img_morph, img_morph, dilateKernel2);
 
             //NOTE: contours
@@ -186,11 +229,8 @@ public class NewSampleSearch implements VisionProcessor {
             }
             if (seeThres) {
                 Imgproc.cvtColor(img_threshold, img_threshold, COLOR_GRAY2RGBA);
-                Core.addWeighted(frame, 0.8, img_threshold, 0.25, 0.1, frame);
-                Imgproc.putText(frame, String.format("frame, thres: %s, %s", frame.channels(), img_threshold.channels()),
-                        new Point(200,300),
-                        Imgproc.FONT_HERSHEY_SIMPLEX,
-                        0.5, new Scalar(255, 255, 255), 1, Imgproc.LINE_AA);
+                Core.addWeighted(frame, 0.6, img_threshold, 2, 0.1, frame);
+
             }
             if (seeMorph)
                 img_morph.copyTo(frame);
